58ab97b93757be1e07fa69f046aee8df408aa95a
tmack1989 <mack.troy2017@gmail.com>
2021-07-14 15:00:44 -0600

    Pushing up first homework

---
+++ homework.js
@@ -0,0 +1,25 @@
+var x = 4;
+var y = 5;
+console.log(x+y)
+
+var a = 10;
+var b = 3;
+console.log(a-b)
+
+var l = 8;
+var m = 2;
+console.log(l/m)
+
+var p = 1;
+var q = 6;
+console.log(p*q)
+
+var hello = "hello";
+var world = " world";
+var result = hello + world;
+console.log(result)
+
+var fav = "my favorite food is";
+var food = " ice cream";
+var result = fav + food;
+console.log(result)
